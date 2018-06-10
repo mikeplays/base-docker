339bf7c092e34287e2a82b3de5c505cc1590fe85
Mike Whiting <mikejw3@gmail.com>
2018-06-10 09:30:04 +0100

    add db logging

--- docker.sh
+++ docker.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+rm -rf db/log/*.log
+touch db/log/mysql.log
+touch db/log/mysql_error.log
+touch db/log/mysql_slow.log
+chown mike:mike db/log/*.log
+chmod 766 db/log/*.log
+
+
+docker-compose down
 docker-compose up -d
+
+sleep 10
+
+docker exec -t mp_db chown -R mysql:mysql /var/log/mysql
+docker exec -t mp_app php ./vendor/bin/empathy --mysql populate
+
